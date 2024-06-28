2c9219d1e7509b23d072a7fc7fefafec2073afb9
Ritwik Biswas <biswas.ritwik2001@gmail.com>
2024-06-28 15:25:23 +0530

    React Router

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color,setColor]=useState("Olive")
+
+  return (
+    <>
+    <div className="w-full h-screen duration-200" style={{background:color}}>
+    
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+      <button onClick={() =>setColor("red")}className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"red"}}>
+      Red
+      </button>
+      <button onClick={() =>setColor("Pink")}className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"pink"}}>
+      Pink
+      </button>
+      <button onClick={() =>setColor("green")} className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"green"}}>
+      Green
+      </button>
+      <button onClick={() =>setColor("blue")}className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"blue"}}>
+      Blue
+      </button>
+      <button onClick={() =>setColor("yellow")}className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"yellow"}}>
+      Yellow
+      </button>
+      <button onClick={() =>setColor("grey")}className="outline-none px-4 py-1 rounded-none text-black shadow-lg" style={{backgroundColor:"grey"}}>
+      Grey
+      </button>
+
+      </div>
+
+      </div>
+    </div>
+
+      
+    </>
+  )
+}
+
+export default App
